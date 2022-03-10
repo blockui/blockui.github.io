494ae8059de5b21d65732fcb3a003e1bf58d459e
bangde2021 <pl20200626@gmail.com>
2022-03-10 12:29:37 +0800

    init

--- src/pages/Wallet/shared/api.js
+++ src/pages/Wallet/shared/api.js
@@ -2,7 +2,7 @@ export const Api = (path) => {
   const t = window.__BASE_API.split("|")
   console.log("process.env.NODE_ENV", process.env.NODE_ENV)
   let baseApi = t[0] === "{{BASE_API}}" ? t[1] : t[0]
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV === "production") {
     baseApi = "https://bangde.xyz/api"
   }
   return baseApi + path
