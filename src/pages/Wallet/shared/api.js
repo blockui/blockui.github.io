94319c2028490f370ccc506601f782666d5f23d6
bangde2021 <pl20200626@gmail.com>
2022-03-10 12:07:33 +0800

    init

--- src/pages/Wallet/shared/api.js
+++ src/pages/Wallet/shared/api.js
@@ -2,8 +2,8 @@ export const Api = (path) => {
   const t = window.__BASE_API.split("|")
   console.log("process.env.NODE_ENV", process.env.NODE_ENV)
   let baseApi = t[0] === "{{BASE_API}}" ? t[1] : t[0]
-  if (process.env.NODE_ENV !== "development") {
-    baseApi = "https://bangde.xyz"
+  if (process.env.NODE_ENV !== "production") {
+    baseApi = "https://bangde.xyz/api"
   }
   return baseApi + path
 }
